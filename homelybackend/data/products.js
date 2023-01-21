08f394b4b801573bbc68449a569dded67002202a
barakaganai <barakaganai01@gmail.com>
2023-01-21 13:08:38 +0300

    create products.js

---
+++ homelybackend/data/products.js
@@ -0,0 +1,57 @@
+const products = [
+  {
+    name: "Yam and egg sauce",
+    description:
+      "Egg sauce and boiled yam is one of the breakfast staples in Nigeria or should I say weekend breakfast. Last Saturday, Olu made breakfast and I thought I would share with you my bae’s yam and egg sauce.  Truth be told, he makes the best and I don't even argue when we are in the egg sauce department.",
+    price: 5000.0,
+    imageURL:
+      "https://storage.dado.ng/wp-content/uploads/2019/06/06200804/Webp.net-resizeimage-3.png",
+    countInStock: 50,
+  },
+  {
+    name: "Jollof rice and chicken",
+    description:
+      "Everyone needs a good chicken and rice recipe they can make whether for dinner or for special occasions. This Jollof Rice With Chicken recipe is that kind of recipe. It is a keeper! Print it out and make it whenever you want to make a rice dish that has the wow factor.",
+    price: 6000.0,
+    imageURL:
+      "https://www.preciouscore.com/wp-content/uploads/2021/12/Jollof-Rice-With-Chicken-500x500.jpg",
+    countInStock: 50,
+  },
+  {
+    name: "Porridge beans",
+    description:
+      "Nigerian beans porridge is the definition of complete comfort and can be paired with garri (cassava flakes), bread or ogi (fermented, corn starch porridge)",
+    price: 2000.0,
+    imageURL:
+      "https://www.thepretendchef.com/wp-content/uploads/2017/11/nigerian-beans-porridge-1-7.jpg",
+    countInStock: 50,
+  },
+  {
+    name: "Semo and egusi soup",
+    description:
+      "Semo, also called semolina fufu, is a Nigerian swallow eaten with various soups, such as egusi soup, okro soup, efọ̀ riri, and Nigerian èfirin soup. It is nutritious and starchy.",
+    price: 4000.0,
+    imageURL:
+      "https://ntachiosa.com/wp-content/uploads/2020/08/ntachi-2-1110x1110-eited2-.png",
+    countInStock: 50,
+  },
+  {
+    name: "Amala and ewedu soup",
+    description:
+      "Amala and Ewedu soup is one of the most popular Nigerian soups, not only among the Yoruba people. Amala is a traditional Nigerian dish.",
+    price: 8000.0,
+    imageURL: "https://pbs.twimg.com/media/Ey9RpckWQAIzcXj.jpg",
+    countInStock: 50,
+  },
+  {
+    name: "Eba and okra soup",
+    description:
+      "Okra soup (African okro soup) is made with okra vegetables cooked in a delicious mixture of palm oil, shrimps, goat meat, fish, and African spices and simmered to perfection! This loaded okro soup is a staple in West Africa, including Nigeria, Ghana, and Cameroon.",
+    price: 3500.0,
+    imageURL:
+      "https://allnigerianfoods.com/wp-content/uploads/okro_soup_recipe.jpg",
+    countInStock: 50,
+  },
+];
+
+module.exports = products;
