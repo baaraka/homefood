1d5b3ebb66824d784146a51e949b63f3875be742
barakaganai <barakaganai01@gmail.com>
2023-01-31 01:13:28 +0300

    add useState hooks

--- homelyfrontend/src/topBar/TopBar.js
+++ homelyfrontend/src/topBar/TopBar.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../modal/Modal";
 import "./TopBar.css";
 
-export default function TopBar() {
+export default function TopBar({ cart }) {
   const [show, setShow] = useState(false);
   const [modal, setModal] = useState(false);
+
+  const [CART, setCART] = useState([]);
+  useEffect(() => {
+    setCART(cart);
+  }, [cart]);
 
   return (
     <div className="topBar">
@@ -36,42 +41,85 @@ export default function TopBar() {
             <i class="topBarCartL fa-solid fa-cart-shopping"></i>
           </span>
           Cart
-          <span className="topInput">0</span>
+          <span className="topInput">{cart.length}</span>
         </button>
       </div>
       <Modal trigger={modal} setTrigger={setModal} className="popupContainer">
         <div className="popupContent">
           <div className="containerBody">
-            <div className="modalBodyDiv">
-              <div className="dishImage">
-                <img
-                  src="https://storage.dado.ng/wp-content/uploads/2019/06/06200804/Webp.net-resizeimage-3.png"
-                  alt=""
-                  className="modalImg"
-                />
-              </div>
-              <div className="dishElite">
-                <p className="dishParagraphImg">Yam and egg sauce</p>
-                <div className="divDiv">
-                  <p>
-                    <span className="dishNumberList">TZS 5000.00/=</span>
-                  </p>
-
-                  <div className="qtyList">
-                    <p>
-                      <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                      </select>
-                    </p>
+            {CART?.map((cartItem, cartIndex) => {
+              return (
+                <div className="modalBodyDiv" key={cartIndex}>
+                  <div className="dishImage">
+                    <img src={cartItem.imageURL} alt="" className="modalImg" />
                   </div>
-                  <button className="deleteBtn">
-                    <i className="deleteIcon fa-solid fa-trash"></i>
-                  </button>
+                  <div className="dishElite">
+                    <p className="dishParagraphImg">{cartItem.name}</p>
+                    <div className="divDiv">
+                      <p>
+                        <span className="dishNumberList">
+                          TZS {cartItem.price * cartItem.quantity}
+                        </span>
+                      </p>
+
+                      <div className="qtyList">
+                        <button
+                          onClick={() => {
+                            const _CART = CART.map((item, index) => {
+                              return cartIndex === index
+                                ? {
+                                    ...item,
+                                    quantity:
+                                      item.quantity > 0 ? item.quantity - 1 : 0,
+                                  }
+                                : item;
+                            });
+                            setCART(_CART);
+                          }}
+                        >
+                          -
+                        </button>
+                        <span>{cartItem.quantity}</span>
+                        <button
+                          onClick={() => {
+                            const _CART = CART.map((item, index) => {
+                              return cartIndex === index
+                                ? { ...item, quantity: item.quantity + 1 }
+                                : item;
+                            });
+                            setCART(_CART);
+                          }}
+                        >
+                          +
+                        </button>
+                      </div>
+                      <button className="deleteBtn">
+                        <i className="deleteIcon fa-solid fa-trash"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+            <div className="popupHeaderBtn">
+              <div className="divBtn">
+                <div className="totalAmount">
+                  <p className="totalAmt">Total Amount</p>
+                </div>
+                <div className="totalAmounts">
+                  <p className="amtTotal">
+                    TZS{" "}
+                    {CART?.map((item) => item.price * item.quantity).reduce(
+                      (total, value) => total + value,
+                      0
+                    )}
+                  </p>
                 </div>
               </div>
+              <button onClick={() => setModal(false)} className="closeButton">
+                Close
+              </button>
+              <button className="orderButton">Order</button>
             </div>
           </div>
         </div>
@@ -106,7 +154,7 @@ export default function TopBar() {
               <i class="topBarCartL fa-solid fa-cart-shopping"></i>
             </span>
             Cart
-            <span className="topInput">0</span>
+            <span className="topInput">{cart.length}</span>
           </button>
         </div>
       ) : null}
