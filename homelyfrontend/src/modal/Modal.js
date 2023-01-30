3924abb9bbb8e44e3b70922f4bc7f0284830f6c6
barakaganai <barakaganai01@gmail.com>
2023-01-31 01:11:45 +0300

    remove div

--- homelyfrontend/src/modal/Modal.js
+++ homelyfrontend/src/modal/Modal.js
@@ -6,23 +6,6 @@ export default function Modal(props) {
       <div className="popupContainer">
         <div className="popupHeader">
           <div className="popupH"> {props.children}</div>
-          <div className="popupHeaderBtn">
-            <div className="divBtn">
-              <div className="totalAmount">
-                <p className="totalAmt">Total Amount</p>
-              </div>
-              <div className="totalAmounts">
-                <p className="amtTotal">TZS 50000.00</p>
-              </div>
-            </div>
-            <button
-              onClick={() => props.setTrigger(false)}
-              className="closeButton"
-            >
-              Close
-            </button>
-            <button className="orderButton">Order</button>
-          </div>
         </div>
       </div>
     </div>
